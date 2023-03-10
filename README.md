b9a06307d84af0f25f195683f808d2a47c95ce1c
zocsen <zocsen@gmail.com>
2023-03-10 19:11:24 +0100

    Last

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-https://zocsen.github.io/ForMum/
+# First verison of the front-end site, only html, css & js
+
+# https://zocsen.github.io/CsodaAsvanyok_BASE/
--- assets/js/random.js
+++
@@ -1,4 +0,0 @@
-var userInput = prompt("Tell us something: ");
-alert("You have written " + userInput.length + " characters"); 
-var slicedUserInput = userInput.slice(0, 10);
-alert(slicedUserInput);
